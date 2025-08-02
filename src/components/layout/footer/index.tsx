b5c84e06f7d268b9a6c095bf94719fcc120e38cd
Yasin Gökhan Erdoğan <yasingokhanerdogan@gmail.com>
2025-08-02 16:17:47 +0300

    feat: add footer

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import { HeartIcon } from "lucide-react";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-full max-w-7xl mx-auto h-14">
+      <div className="relative h-full flex items-center justify-between text-muted-foreground/75 px-8">
+        <p className="font-medium">&copy; {new Date().getFullYear()} YGE. All rights reserved.</p>
+        <div className="flex items-center gap-4">
+          <Link href="/" className="flex items-center gap-1 [&_svg]:size-4">
+            Built with <HeartIcon className="text-destructive fill-destructive -mt-1" /> by{" "}
+            <span className="text-primary font-semibold">YGE</span>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
