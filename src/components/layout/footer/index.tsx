c16f8c0f273595195245c9107f4133eb4727693e
Yasin Gökhan Erdoğan <yasingokhanerdogan@gmail.com>
2025-08-02 16:54:37 +0300

    feat: add service page

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -7,7 +7,12 @@ export default function Footer() {
       <div className="relative h-full flex items-center justify-between text-muted-foreground/75 px-8">
         <p className="font-medium">&copy; {new Date().getFullYear()} YGE. All rights reserved.</p>
         <div className="flex items-center gap-4">
-          <Link href="/" className="flex items-center gap-1 [&_svg]:size-4">
+          <Link
+            href="https://www.yasingokhanerdogan.com?utm_source=yge-interior-design-template"
+            className="flex items-center gap-1 [&_svg]:size-4"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             Built with <HeartIcon className="text-destructive fill-destructive -mt-1" /> by{" "}
             <span className="text-primary font-semibold">YGE</span>
           </Link>
